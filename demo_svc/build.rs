863e3dc413a83f486413070eea6dbad6f50c0c69
bruno-medeiros <bruno.do.medeiros@gmail.com>
2025-05-02 20:15:49 +0100

    sudo apt-get

--- demo_svc/build.rs
+++ demo_svc/build.rs
@@ -1,18 +1,6 @@
-use std::process::Command;
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Setup deps via Docker
-    println!("cargo::rerun-if-changed=docker-compose.yml");
 
-    let output = Command::new("docker")
-        .args(["compose", "-f", "../docker-compose.yml", "up", "-d"])
-        .spawn()?
-        .wait_with_output()?;
-
-    if !output.status.success() {
-        return Err("docker-compose failed".into());
-    }
-
+    // TODO: only set if not set already...
     let db_url = "postgres://postgres:mypassword@localhost:5432/postgres";
     println!("cargo:rustc-env=DATABASE_URL={db_url}");
 
