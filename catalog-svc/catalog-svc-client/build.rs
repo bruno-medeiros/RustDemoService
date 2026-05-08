f7ad3589354027f98a4b152e422c9d0226d0d2c0
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2026-05-08 16:06:18 +0100

    actually add lints to crates

--- catalog-svc/catalog-svc-client/build.rs
+++ catalog-svc/catalog-svc-client/build.rs
@@ -1,16 +1,17 @@
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let src = "../openapi30.json";
     println!("cargo:rerun-if-changed={}", src);
-    let file = std::fs::File::open(src).unwrap();
-    let spec = serde_json::from_reader(file).unwrap();
+    let file = std::fs::File::open(src)?;
+    let spec = serde_json::from_reader(file)?;
     let mut generator = progenitor::Generator::default();
 
-    let tokens = generator.generate_tokens(&spec).unwrap();
-    let ast = syn::parse2(tokens).unwrap();
+    let tokens = generator.generate_tokens(&spec)?;
+    let ast = syn::parse2(tokens)?;
     let content = prettyplease::unparse(&ast);
 
-    let mut out_file = std::path::Path::new(&std::env::var("OUT_DIR").unwrap()).to_path_buf();
+    let mut out_file = std::path::Path::new(&std::env::var("OUT_DIR")?).to_path_buf();
     out_file.push("codegen.rs");
 
-    std::fs::write(out_file, content).unwrap();
+    std::fs::write(out_file, content)?;
+    Ok(())
 }
